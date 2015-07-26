1e34733f20e25ebb74fd6ea733b3a943edf8bf12
suren-vijayakumar <suren.vijayakumar@gmail.com>
2015-07-26 15:24:33 -0500

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+ var searchGithub;
 $(document).ready(function(){
-  
+	
+	$("body").on('click', '.gititButton', function(event){
+			event.preventDefault();
+			searchGithub = $('#input').val();
+			search(searchGithub);
+		});
+
+});
+	
+
+
+function search(query){
 	$.ajax({
-		url:'https://api.github.com/users/suren-vijayakumar?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d', 
+		url:'https://api.github.com/users/'+ searchGithub + '?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d', 
 		type:'GET',
 		dataType:'json',
 		crossDomain:true,
@@ -10,10 +22,11 @@ $(document).ready(function(){
 			callback(data);
 		}
 	});
+}
 	 function callback(data){
        
-$("#results").append(data.login + ',');
-
+$("#resultsName").append("Name: " + data.name);
+$("#resultsLink").append("Link: <a href='"+data.html_url+"'target='blank'>"+data.html_url+"</a>");
+$("#resultsPhoto").append("Looks like: <img src='"+data.avatar_url+"' />");
 }
        
-});
